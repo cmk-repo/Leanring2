5e3f20f621214d432522bf9dd814afa7b94d030d
chan <contact@chandan.fyi>
2024-01-15 16:10:18 -0500

    recoil

--- week-7-2/src/App.jsx
+++ week-7-2/src/App.jsx
@@ -1,9 +1,58 @@
+import { useContext, useMemo, useState } from "react"
+import { CountContext } from "./context";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
+
 function App() {
   return (
-    <>
-      <div>Hello</div>
-    </>
-  );
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </div>
+  )
 }
 
-export default App;
+function Count() {
+  console.log("re-render");
+  return <div>
+    <CountRenderer />
+    <Buttons />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+
+  return <div>
+    <b>
+      {count}
+    </b>
+    <EvenCountRenderer />
+  </div>
+}
+
+function EvenCountRenderer() {
+  const isEven = useRecoilValue(evenSelector);
+
+  return <div>
+    {isEven ? "It is even" : null}
+  </div>
+}
+
+function Buttons() {
+  const setCount = useSetRecoilState(countAtom);
+  console.log("buttons re-rendererd");
+
+  return <div>
+    <button onClick={() => {
+      setCount(count => count + 1)
+    }}>Increase</button>
+
+    <button onClick={() => {
+      setCount(count => count - 1)
+    }}>Decrease</button>
+  </div>
+}
+
+export default App
