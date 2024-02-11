368bcff2afdf2bed14df17f1eb3b24686a4953e0
cmk <contact@chandan.fyi>
2024-02-10 22:13:16 -0500

    paytm clone

--- PAYTM-CLONE/backend/db.js
+++ PAYTM-CLONE/backend/db.js
@@ -1,9 +1,7 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
-//mongoose.connect("mongodb://localhost:27017/paytm")
-
-mongoose.connect("mongodb+srv://cmk:d6rngRn._BTGY9w@cluster0.4fxjiz5.mongodb.net/")
+mongoose.connect("mongodb://localhost:27017/paytm")
 
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
--- PAYTM-CLONE/backend/routes/account.js
+++ PAYTM-CLONE/backend/routes/account.js
@@ -21,9 +21,11 @@ router.post("/transfer", authMiddleware, async (req, res) => {
 
     session.startTransaction();
     const { amount, to } = req.body;
+    console.log(req.userId)
 
     // Fetch the accounts within the transaction
     const account = await Account.findOne({ userId: req.userId }).session(session);
+    console.log("here233")
 
     if (!account || account.balance < amount) {
         await session.abortTransaction();
@@ -31,9 +33,9 @@ router.post("/transfer", authMiddleware, async (req, res) => {
             message: "Insufficient balance"
         });
     }
-
+    console.log("here2")
     const toAccount = await Account.findOne({ userId: to }).session(session);
-
+    console.log("here3")
     if (!toAccount) {
         await session.abortTransaction();
         return res.status(400).json({
@@ -42,11 +44,14 @@ router.post("/transfer", authMiddleware, async (req, res) => {
     }
 
     // Perform the transfer
+
+
     await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
     await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
-
+    console.log("here4")
     // Commit the transaction
     await session.commitTransaction();
+    console.log("here45")
     res.json({
         message: "Transfer successful"
     });
